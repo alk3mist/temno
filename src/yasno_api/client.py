d58a5b4365e7c5ca99dbc322c7ac85697d2e0eec
Oleksandr Havrylin <alexandr.gavrilin@hotmail.com>
2024-12-26 20:17:56 +0200

    Add HTTP caching for the Yasno API client

--- src/yasno_api/client.py
+++ src/yasno_api/client.py
@@ -1,17 +1,26 @@
-import httpx
-from model import ScheduleResponse
+import hishel
+from model import ScheduleComponent, ScheduleResponse
 
 SCHEDULE_URL = (
     "https://api.yasno.com.ua/api/v1/pages/home/schedule-turn-off-electricity"
 )
 
 
-def fetch_schedule() -> ScheduleResponse:
-    response = httpx.get(SCHEDULE_URL)
+def fetch_schedule() -> ScheduleComponent:
+    with hishel.CacheClient() as client:
+        response = client.get(SCHEDULE_URL)
     response.raise_for_status()
     response_json = response.json()
-    return ScheduleResponse.model_validate(response_json)
+    return ScheduleResponse.model_validate(response_json).schedule
 
 
 if __name__ == "__main__":
-    print(fetch_schedule())
+    schedule = fetch_schedule()
+    print(f"updated at: {schedule.updated_at}")
+    dnipro = schedule.daily_schedule["dnipro"]
+    print(f"today: {dnipro.today.title}")
+    print("and")
+    if dnipro.tomorrow:
+        print(f"tomorrow: {dnipro.tomorrow.title}")
+    else:
+        print("no tomorrow(")
