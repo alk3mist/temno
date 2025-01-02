f7bd5aa81aa80ec785fef8aa75b922b381ac8534
Oleksandr Havrylin <oleksa.havrylin@outlook.com>
2025-01-03 00:03:09 +0200

    Refactor tests

--- tests/temno/test_map_yasno.py
+++ tests/temno/test_map_yasno.py
@@ -1,6 +1,4 @@
 from datetime import time
-from functools import partial
-from itertools import starmap
 
 import pytest
 
@@ -13,53 +11,48 @@ from yasno_api import schema as yasno
     [
         (
             [
-                (8.0, 8.5),
-                (8.5, 9.0),
-                (9.0, 9.5),
-                (19.0, 19.5),
-                (19.5, 20.0),
-                (20.0, 20.5),
-                (20.5, 21.0),
-                (23.5, 24.0),
+                (7.5, 8.0, "DEFINITE_OUTAGE"),
+                (8.0, 8.5, "DEFINITE_OUTAGE"),
             ],
             [
-                (time(8, 00), time(9, 30)),
-                (time(19, 00), time(21, 00)),
-                (time(23, 30), time(0, 00)),
+                (time(7, 30), time(8, 30), "DEFINITE_OUTAGE"),
+            ],
+        ),
+        (
+            [
+                (7.0, 8.5, "DEFINITE_OUTAGE"),
+                (8.5, 9.0, "POSSIBLE_OUTAGE"),
+            ],
+            [
+                (time(7, 00), time(8, 30), "DEFINITE_OUTAGE"),
+                (time(8, 30), time(9, 00), "POSSIBLE_OUTAGE"),
+            ],
+        ),
+        (
+            [
+                (9.0, 9.5, "POSSIBLE_OUTAGE"),
+                (14.0, 14.5, "POSSIBLE_OUTAGE"),
+                (14.5, 15.0, "POSSIBLE_OUTAGE"),
+            ],
+            [
+                (time(9, 00), time(9, 30), "POSSIBLE_OUTAGE"),
+                (time(14, 00), time(15, 00), "POSSIBLE_OUTAGE"),
             ],
         ),
         ([], []),
     ],
 )
 def test_events_to_model_events(
-    values: list[tuple[float, float]], result: list[tuple[time, time]]
-):
-    yasno_events = _make_yasno_events(values)
-    model_events = map_yasno.events_to_model_events(yasno_events)
-    assert list(model_events) == list(_make_model_events(result))
-
-
-@pytest.mark.parametrize(
-    "values,result",
-    [
-        (
-            [
-                (8, 8.5, "DEFINITE_OUTAGE"),
-                (8.5, 9.0, "POSSIBLE_OUTAGE"),
-                (9.0, 9.5, "POSSIBLE_OUTAGE"),
-            ],
-            [
-                (time(8, 00), time(8, 30), "DEFINITE_OUTAGE"),
-                (time(8, 30), time(9, 30), "POSSIBLE_OUTAGE"),
-            ],
-        ),
-    ],
-)
-def test_events_to_model_events_different_types(
     values: list[tuple[float, float, yasno.EventType]],
     result: list[tuple[time, time, model.EventType]],
-): ...
-
-
-_make_yasno_events = partial(starmap, yasno.OutageEvent.create_definite)
-_make_model_events = partial(starmap, model.OutageEvent.create_definite)
+):
+    yasno_events = (
+        yasno.OutageEvent(start=start, end=end, type=type_)
+        for (start, end, type_) in values
+    )
+    events = map_yasno.events_to_model_events(yasno_events)
+    expected = [
+        model.OutageEvent(start=start, end=end, type=type_)
+        for (start, end, type_) in result
+    ]
+    assert list(events) == expected
